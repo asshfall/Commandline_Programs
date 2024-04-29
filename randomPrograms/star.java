3654b36496f0110aa6567d46afbf2b01ca51406b
Asshley <asshleydeanwaynegozar@gmail.com>
2024-04-29 14:34:10 +0800

    star Program updated

--- randomPrograms/star.java
+++ randomPrograms/star.java
@@ -3,11 +3,11 @@ public class star {
 
     public static void main(String[]args){
 
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= 9; i++){
 
             for(int j = 1; j <= i; j++){
 
-                 System.out.print("*");
+                 System.out.print(i);
                  
             }
 
